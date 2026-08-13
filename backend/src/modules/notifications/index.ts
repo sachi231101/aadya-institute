275d7ecac77034fa636f49e9d256dc633137915c
Hareeshanv <153485744+Hareeshanv@users.noreply.github.com>
2026-08-13 19:25:45 +0530

    Add files via upload

---
+++ backend/src/modules/notifications/index.ts
@@ -0,0 +1,5 @@
+export * from "./notification.types";
+export * from "./notification.repository";
+export * from "./notification.service";
+export * from "./notification.controller";
+export { default as notificationRoutes } from "./notification.routes";
