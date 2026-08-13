b137352e4df07de7b5c48c8c8acfcf02f4ea2261
sachi231101 <119731144+sachi231101@users.noreply.github.com>
2026-08-13 19:18:42 +0530

    Delete backend/src/modules/notifications directory

--- backend/src/modules/notifications/index.ts
+++
@@ -1,8 +0,0 @@
-export * from "./notification.service";
-export * from "./notification.constants";
-export * from "./notification.types";
-export * from "./notification.types";
-export * from "./notification.repository";
-export * from "./notification.service";
-export * from "./notification.controller";
-export { default as notificationRoutes } from "./notification.routes";
